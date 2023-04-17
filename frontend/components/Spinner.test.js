e22deea006cf99cf5dc4d9ca89ddec336d0e3130
Jordan~ <jordan.brewer509@gmail.com>
2023-04-17 13:59:43 -0700

    userTests passing

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,16 @@
 // Import the Spinner component into this file and test
+import React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import Spinner from "./Spinner"
+
 // that it renders what it should for the different props it can take.
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('spinner works', () => {
+  render(<Spinner on={true}/>)
+  const text = screen.getByText('Please wait...')
+  waitFor(() => {expect(text).toBeInTheDocument()})
 })
